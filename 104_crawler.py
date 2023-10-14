4ea6bbc7c72c5e7b74cc854c7ae8cfd215b7f3d2
LaloSalamanka <140996895+LaloSalamanka@users.noreply.github.com>
2023-10-14 19:33:24 +0800

    v3
    爬104網站, 用selenium搜尋java, 點選全職, 將150頁的內容全部抓下來且匯入到mysql資料庫中, 總共抓取3000筆資料.
    
    #其實筆數是超過3000筆的, 但是如沒有登入的話104只會顯示150頁
    #有發現登入後利用配對條件去搜索, 頁數會根據筆數去調整. 但無奈利用selenium登入的話, 網頁會要求回傳驗證碼, 只好作罷

--- 104_crawler.py
+++ 104_crawler.py
@@ -1,42 +1,18 @@
-# 從opendata或網路爬蟲擷取資料,並將資料存入資料庫
-# 從資料庫讀取資料並進行資料分析與視覺化
-#  以簡報方式報告您的專題
-    #  選取動機
-    #  圖表說明
-    #  遇到的問題與解決方式
-#  繳交資料:
-    #  PPT簡報
-    #  來源資料(CSV、JSON、XML皆可)
-    #  Python程式碼
-
-# 104職缺分析
-    # 爬取所有軟體工程師的職缺
-    # 全台灣地區軟體工程師職缺分布
-    # 所有java的職缺
-    # 所有python的職缺
-    # java跟python在資訊軟體系統類佔了多少比例
-    # 哪個地區最多職缺
-    # 哪個地區最少職缺
-    # 最後選出適合去哪邊工作之類的
-# 第一步: 先把資料爬出來
-# 第二步: 看怎麼存到資料庫裡面
-# 第三步: 再從資料庫讀取資料進行分析與資料視覺化
-# 第四步: 邊做報告邊用
 import requests
 from bs4 import BeautifulSoup
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 import time
-import json
 import mysql.connector
 import pymysql
 def fetch_data(url):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url)
 
     soup = BeautifulSoup(response.text, "html.parser")
     articles = soup.find_all("article", class_="b-block--top-bord job-list-item b-clearfix js-job-item")
@@ -78,7 +54,7 @@ def fetch_data(url):
             company_required_experience = company_required_experience.text.strip()
             company_required_education = company_required_education.text.strip()
         else:
-            print("N/A")
+            company_required_education = company_required_experience = company_district = "N/A"
 
         # 發布日期
         company_release_date = block_left.find("h2", class_="b-tit")
@@ -86,22 +62,14 @@ def fetch_data(url):
         if company_release_date:
             company_release_date = company_release_date.text.strip()
         else:
-            print("N/A")
+            company_release_date = "N/A"
 
         # 應徵人數
         company_applicants = a.find("div", class_="b-block__right b-pos-relative")
         if company_applicants:
             company_applicants = company_applicants.a.text
         else:
-            print("N/A")
-
-        print(f"""
-        {company_release_date} {company_applicants}
-        職缺名稱: {job_title}
-        公司名稱: {company_name}
-        公司產業: {company_industry}
-        公司區域: {company_district}
-        要求學經歷: {company_required_education}, {company_required_experience}""")
+            company_applicants = "N/A"
 
         # 連接到 MySQL 資料庫
         try:
@@ -135,7 +103,7 @@ def fetch_data(url):
 
         # 插入資料
         cursor.execute('''
-                    INSERT INTO jobs (job_title, company_name, company_industry, company_district, company_required_education, company_required_experience, company_release_date, company_applicants)
+                    INSERT INTO 104crawler (job_title, company_name, company_industry, company_district, company_required_education, company_required_experience, company_release_date, company_applicants)
                     VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
                 ''', (job_title, company_name, company_industry, company_district, company_required_education,
                       company_required_experience, company_release_date, company_applicants))
@@ -147,57 +115,48 @@ def fetch_data(url):
         cursor.close()
         conn.close()
 
-url = 'https://www.104.com.tw/jobs/search/?cat=2007000000&jobsource=2018indexpoc&ro=0'
-fetch_data(url)
-
-# 寫到資料庫
-
-
-
-
-
 # 下一頁的邏輯
+def nextpage(page):
+    # 設定Chrome Driver的執行檔路徑
+    options = Options()
+    options.add_experimental_option("detach", True)
+    options.chrome_executable_path = r"C:\Users\austi\PycharmProjects\pythonCrawler\chromedriver.exe"
 
-# 創建一個 WebDriver
-# driver = webdriver.Chrome()
-#
-# # 打開網頁
-# driver.get("https://www.104.com.tw/jobs/search/?cat=2007000000&jobsource=2018indexpoc&ro=0")
-#
-# # 使用 Selenium 定位並操作下拉式選單
-# dropdown = WebDriverWait(driver, 10).until(
-#     EC.presence_of_element_located(
-#         (By.CSS_SELECTOR,
-#          '#main-content .b-float-right label.b-select select.page-select.js-paging-select.gtm-paging-top'))
-# )
-# select = Select(dropdown)
-#
-# # 選擇特定頁數（這裡是第 page 頁）
-# select.select_by_value("3")
-#
-# # 等待頁面跳轉完畢
-# time.sleep(1)  # 這裡可以調整等待的時間
-#
-# # 獲取當前頁面的網址
-# current_url = driver.current_url
-#
-# # 呼叫fetch_data並帶入當前網址
-# fetch_data(current_url)
+    # 建立Driver物件實體，用程式操作瀏覽器運作
+    driver = webdriver.Chrome(options=options)
 
-    # 準備第二步 怎麼存到資料庫
-# for a in articles:
-#     title = a.find("h2", class_="b_tit")
-#     for b in title:
-#         title2 = b.find("a", class_="js-job-link")
-#         print(title2.b.text)
+    # 打開網頁
+    driver.get("https://www.104.com.tw/jobs/main/")
 
-# if response.status_code == 200:
-#     soup = BeautifulSoup(response.text, 'html.parser')
-#     title = soup.title.text
-#     print('寫入成功')
-#     with open('output.html', 'w', encoding='utf-8') as f:
-#         f.write(response.text)
-# else:
-#     print(f'Error: {response.status_code}')
+    keywordInput = driver.find_element(By.ID, "ikeyword")
+    keywordInput.send_keys("java")
+    time.sleep(1)
+    button = driver.find_element(By.CSS_SELECTOR, ".btn.btn-primary.js-formCheck")
+    button.send_keys(Keys.RETURN)
+
+    # 使用 Selenium 定位並操作下拉式選單
+    dropdown = WebDriverWait(driver, 60).until(
+        EC.presence_of_element_located(
+            (By.CLASS_NAME,
+             'page-select js-paging-select gtm-paging-top'))
+    )
+    select = Select(dropdown)
+
+    # 選擇特定頁數（這裡是第 page 頁）
+    select.select_by_value(str(page))
+
+    # 等待頁面跳轉完畢
+    time.sleep(1)  # 這裡可以調整等待的時間
+
+    # 獲取當前頁面的網址
+    current_url = driver.current_url
+
+    # 呼叫fetch_data並帶入當前網址
+    fetch_data(current_url)
+    # driver.close()
+
+for i in range(1, 3):
+    print(f"正在爬第{i}頁")
+    nextpage(i)
 
 
